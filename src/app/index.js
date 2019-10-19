431d0713fb9e0bc1cbc29c1042c88fcdb33fdfae
Vin_04 <vinayakborhade@gmail.com>
2019-10-20 00:51:54 +0530

    starting with the todo Component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,20 @@
-alert("It works!!")
+var React=require("react");
+var ReactDOM=require("react-dom");
+
+// create component
+class TodoComponent extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>hellooo!!</h1>
+                <p>hey you! yes you!</p>
+            </div>
+        );
+    }
+}
+
+// put the component into html page
+ReactDOM.render(
+    <TodoComponent />,
+    document.getElementById("todo-wrapper")
+);
