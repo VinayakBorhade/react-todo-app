0e94beeb39506d2cd760023b90882b3ef38cf1bb
Vin_04 <vinayakborhade@gmail.com>
2019-10-23 11:23:32 +0530

    working with states

--- src/app/index.js
+++ src/app/index.js
@@ -1,19 +1,41 @@
 var React=require("react");
 var ReactDOM=require("react-dom");
+var createReactClass = require('create-react-class');
 
 // create component
-class TodoComponent extends React.Component{
-    render(){
+var TodoComponent =createReactClass({       //extends React.Component{
+    getInitialState: function(){
+        return{
+            todos: ["task1", "task2", "task3"],
+            age: 30
+        };
+    },
+    render: function(){
+        // let component=this;
+        var age=setTimeout(
+            function(){
+                this.setState({
+                    age: 35
+                });
+            }.bind(this),
+            5000
+        );
         return(
             <div>
                 <p>Your Cheese</p>
                 {this.props.cheese.name} - 
                 {this.props.cheese.price} -
                 {this.props.cheese.quantity}
+                <p>age: {this.state.age}</p>
+                <ul id="todo-list">
+                    <li>{this.state.todos[0]}</li>
+                    <li>{this.state.todos[1]}</li>
+                    <li>{this.state.todos[2]}</li>
+                </ul>
             </div>
         );
     }
-}
+} );
 
 var myCheese={name: "good cheese", price: "1.01", quantity: 15};
 
