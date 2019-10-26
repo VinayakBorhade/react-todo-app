b27b4b4e1c81a9c41bd78030a3bbdd295f6bd9b7
Vin_04 <vinayakborhade@gmail.com>
2019-10-26 18:09:23 +0530

    modularizing code, adding css for components

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,10 @@
 var React=require("react");
 var ReactDOM=require("react-dom");
 var createReactClass = require('create-react-class');
+require("./css/index.css");
+
+// below are the modules required...
+var TodoItem=require("./todoItem");
 
 // create component
 var TodoComponent =createReactClass({       //extends React.Component{
@@ -44,25 +48,6 @@ var TodoComponent =createReactClass({       //extends React.Component{
         });
     }
 });
-
-// react component for todo-item
-var TodoItem=createReactClass({
-    render: function(){
-        return (
-        <li>
-            <div className="todo-item">
-                <span className="item-name">{this.props.item}</span>
-                <span className="item-delete" onClick={this.handleDelete}> X</span>
-            </div>
-        </li>
-        );
-    },
-    // custom functions...
-    handleDelete: function(){
-        this.props.onDelete(this.props.item);
-    }
-});
-
 
 // put the component into html page
 ReactDOM.render(
---
+++ src/app/todoItem.js
@@ -0,0 +1,22 @@
+var React=require("react");
+var createReactClass = require('create-react-class');
+require("./css/todoItem.css");
+
+var TodoItem=createReactClass({
+    render: function(){
+        return (
+        <li>
+            <div className="todo-item">
+                <span className="item-name">{this.props.item}</span>
+                <span className="item-delete" onClick={this.handleDelete}> X</span>
+            </div>
+        </li>
+        );
+    },
+    // custom functions...
+    handleDelete: function(){
+        this.props.onDelete(this.props.item);
+    }
+});
+
+module.exports=TodoItem;
